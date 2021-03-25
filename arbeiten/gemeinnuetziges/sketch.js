17916b805b5bdce551bdd15f61196264483d138e
Paul Schlenzig <paul.schlenzig@lhind.dlh.de>
2021-03-25 07:44:25 +0100

    fix: dirty workaround to avoid unnecessary comma on HCC page

--- arbeiten/gemeinnuetziges/sketch.js
+++ arbeiten/gemeinnuetziges/sketch.js
@@ -13,8 +13,13 @@ var bool=true;
 
 function preload() {
   img = loadImage('../source/arbeiten/gemeinnuetziges/BLM.png');
-  englishText= loadStrings('../source/arbeiten/gemeinnuetziges/englishText.txt');
-  germanText= loadStrings('../source/arbeiten/gemeinnuetziges/germanText.txt');
+  //englishText= loadStrings('../source/arbeiten/gemeinnuetziges/englishText.txt');
+  //germanText= loadStrings('../source/arbeiten/gemeinnuetziges/germanText.txt');
+  englishText = `As individuals and as an art and cultural association, we would be nothing without those sisters and brothers who struggle with institutional racism every day. Worldwide discrimination and racism, of which the murder of George Floyd in Minneapolis is only the tip of the iceberg, is never just a problem for those affected. We are privileged to address systematic grievances - especially in Germany - and demand change.<br><br>We have used our platform to collect donations for the association "ADEFRA" and the "Black Lives Matter" movement by selling a T-shirt designed by <a href="https://www.instagram.com/808textil/" target="_blank">808TEXTIL</a>. The entire profit of the campaign went to both organisations. The shirt is a tribute to Bashar Barakah Jackson - better known as Pop Smoke - who died far too young this year, and the hiphop culture of the 1990s. The music of Pop Smoke, who was considered a follower of the African-American civil rights activist Malcolm X, has been heard at protests throughout the USA in recent months. His murder in February has still not been solved by the police.`
+
+  germanText = `Als einzelne Menschen sowie als Kunst- und Kulturverein wären wir nichts ohne jene Schwestern und Brüder, die jeden Tag mit institutionellem Rassismus zu kämpfen haben. Weltweit existierende Diskriminierung und Rassismus, bei denen der Mord an George Floyd in Minneapolis nur die Spitze des Eisbergs darstellen, sind niemals nur Problem der Betroffenen. Wir sind privilegiert, systematische Missstände - gerade auch in Deutschland - anzusprechen und Veränderung einzufordern.<br><br>Wir haben unsere Plattform genutzt, um durch den Verkauf eines von <a href="https://www.instagram.com/808textil/" target="_blank">808TEXTIL</a> gestalteten T-Shirts Spenden für den Verein „ADEFRA” und die „Black Lives Matter“ Bewegung zu sammeln. Der gesamte Gewinn der Aktion ging an die beiden Organisationen.Das Shirt ist eine Hommage an den dieses Jahr viel zu jung verstorbenen Bashar Barakah Jackson, besser bekannt als Pop Smoke, und die Hiphop-Kultur der 1990er. Die Musik von Pop Smoke, der als Anhänger des afroamerikanischen Bürgerrechtlers Malcolm X galt, war in den letzten Monaten bei Protesten überall in den USA zu hören. Seine Ermordung im Februar wurde bis heute nicht von der Polizei aufgeklärt.`
+
+
 }
 
 function setup (){
